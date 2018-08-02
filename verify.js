134500931fbf7cd3637dc546fe701cea72f4823c
Yi Liu <liuvictor14@gmail.com>
2018-08-01 22:52:43 -0400

    tested the query from MongoDB and check for valid face values

---
+++ verify.js
@@ -0,0 +1,111 @@
+module.exports.verify = function (code, price, client)
+{
+    //in the form of object, can also be in map form
+    var dictionary = {"SBUX": [5,10,15,20,50], 
+                      "FMSA": [15, 50, 100, 250, 500],
+                      "AMZN": {minimum: 10, maximum: 200},
+                      "AAPL": {minimum: 25, maximum: 300},
+                      "EBAY": [25,50,100,200]};
+    var result = dictionary[code];
+    
+    
+    const dbName = 'brand';
+    /*
+    //the MongoDB client should be passed in and already connected to the database server
+    
+    const MongoClient = require('mongodb').MongoClient;
+    const assert = require('assert');
+    
+    const url = 'mongodb://localhost:27017';
+    MongoClient.connect(url, (err, client) => {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+      var collection = db.collection('values');
+      //collection.find({brand: code}).limit(1)
+      //find the document based on the brand code
+      collection.find({brand: code}, (err1, curs) => {
+          assert.equal(null, err1);
+          console.log("checking the number of matches of brand");
+          curs.toArray((err3, docs) => {
+              console.log(docs.length);
+              console.log(docs);
+              console.log("checking the price for: "+code+" and price = "+price);
+              console.log(check(docs[0], price));
+          });
+          //assert.strictEqual(curs.count(), 1);
+          //curs.next((err2, doc) => {
+          //  assert.equal(null, err2);
+          //  console.log(doc);
+          //})
+          //console.log()
+      });
+      //assert.strictEqual(curs.count(), 1);
+      //curs.next().then();
+      client.close();
+    });
+    */
+    const db = client.db(dbName);
+    var collection = db.collection('values');
+    //collection.find({brand: code}).limit(1)
+    //find the document based on the brand code
+    collection.find({brand: code}, (err1, curs) => {
+        assert.equal(null, err1);
+        console.log("checking the number of matches of brand");
+        curs.toArray((err3, docs) => {
+            console.log(docs.length);
+            console.log(docs);
+            console.log("checking the price for: "+code+" and price = "+price);
+            return check(docs[0], price);
+        });
+    });
+    
+   /*
+    if(result)
+    {
+        if(result instanceof Array)
+        {
+            //discrete gift card values
+            if(result.includes(price))
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            //a range of gift card values
+            if(price >= result.minimum && price <= result.maximum)
+                return true;
+            else
+                return false;
+        }
+    }
+    else
+        return false;
+        */
+}
+function check(result, price)
+{
+    if(result)
+    {
+        if(result instanceof Array)
+        {
+            //discrete gift card values
+            if(result.includes(price))
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            //a range of gift card values
+            if(price >= result.minimum && price <= result.maximum)
+                return true;
+            else
+                return false;
+        }
+    }
+    else
+        return false;
+}
